7c1e16af970740c0f76cd09a1d62c0ff421f414b
Thomas Maurer <thomas.maurer@simplificator.com>
2012-11-06 12:03:14 +0100

    load script added

---
+++ load.js
@@ -0,0 +1,6 @@
+javascript: (function() {
+  s = document.createElement('script');
+  s.setAttribute('type', 'text/javascript');
+  s.setAttribute('src', 'https://raw.github.com/simplificator/basecamp-next-scrum-calculator/master/code.js');
+  document.body.appendChild(s);
+})();
